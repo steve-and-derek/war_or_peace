4ac1cbd81c177e4e35d42371f1afc578119fbebc
Derek Borski <derek.borski@gmail.com>
2020-03-20 08:20:51 -0600

    Cleans up deck_test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -39,7 +39,6 @@ class DeckTest < Minitest::Test
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:club, '5', 5)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
